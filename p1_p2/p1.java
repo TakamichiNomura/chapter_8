6371db2a6ae3f7e9397c675125b3190f606aa8fa
TakamichiNomura <hs19980101@yahoo.co.jp>
2019-10-07 00:05:35 +0900

    filewrite가 잘 안된다. 아~~~~~~ 이거 어떻게 하는거야!

--- p1_p2/p1.java
+++ p1_p2/p1.java
@@ -11,21 +11,20 @@ import java.util.*;
 public class p1
 {
     public static void main(String[] args){
-        PhoneList pl;
         Scanner s = new Scanner(System.in);
         try{
             System.out.println("전화번호 입력 프로그램입니다.");
             while(true){
-                FileWriter fw = new FileWriter("d:\\phone.txt");
+                FileWriter fw = new FileWriter("d:\\iphone.txt");
                 System.out.print("이름 전화번호 >>");
                 String name = s.next();
                 if(name.equals("그만")){
-                    System.out.println("c:\\temp\\phone.txt에 저장하였습니다.");
+                    System.out.println("d:\\iphone.txt에 저장하였습니다.");
                     break;
                 }
                 int phone_num = s.nextInt();
-                pl = new PhoneList(name,phone_num);
-                fw.write(pl.toString());
+                fw.write(name);
+                fw.write(phone_num);
             }
         }catch(IOException e){
             System.out.println("파일이 없습니다.");
--- p1_p2/p2.java
+++ p1_p2/p2.java
@@ -12,10 +12,13 @@ public class p2
 {
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
-        System.out.println("c:\\temp\\phone.txt를 출력합니다.");
+        System.out.println("c:\\temp\\iphone.txt를 출력합니다.");
         try{
-            FileReader fr = new FileReader("d:\\phone.txt");
-            System.out.println(fr.read());
+            FileReader fr = new FileReader("d:\\iphone.txt");
+            int c;
+            while((c = fr.read()) != -1){
+                System.out.print((char)c);
+            }
         }catch(IOException e){
             System.out.println("파일이 없습니다.");
         }
